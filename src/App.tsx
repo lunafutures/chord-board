56a820d122529a61140ab02ae720315e8c9d6007
Luna Futures <luna.futures+github@gmail.com>
2025-03-16 00:21:46 -0500

    Create dummy chord grid full of buttons for each chord

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { JSX } from 'react';
 import './App.css';
 import { MidiNote } from './midi';
 import { Chord, chords } from './chords';
+import _ from 'lodash';
 
 function App() {
   return (
     <div className="App">
-      <ChordButton rootMidiValue={60} chord={chords[4]} preferSharp={true}/>
-      <ChordButton rootMidiValue={61} chord={chords[4]} preferSharp={true}/>
+      <Chords reverseSides={true}/>
     </div>
   );
 }
 
-function ChordButton(props: { rootMidiValue: number, chord: Chord, preferSharp: boolean }) {
+function Chords(props: {reverseSides: boolean}): JSX.Element {
+  const buttons: JSX.Element[] = [];
+  _.range(60, 72).map((rootMidiValue, i) => {
+    chords.map((chord, j) => {
+      buttons.push(
+        <ChordButton
+          key={1}
+          rootMidiValue={rootMidiValue}
+          chord={chord}
+          preferSharp={true}
+          column={props.reverseSides ? i + 1 : j + 1}
+          row={props.reverseSides ? j + 1 : i + 1}
+          />);
+    });
+  });
+  
+  return <div className="chord-grid" style={{display: "grid"}}>
+      {buttons}
+  </div>;
+}
+
+function ChordButton(props: {
+  rootMidiValue: number,
+  chord: Chord,
+  preferSharp: boolean,
+  column: number,
+  row: number,
+}): JSX.Element {
   const midiNote = new MidiNote(props.rootMidiValue);
   const rootString = midiNote.rootString(props.preferSharp);
-  const text = `${rootString}${props.chord.abbreviation}`;
+  const text = `${rootString}${'\u200B'}${props.chord.abbreviation}`;
 
   function mouseDown() {
     console.log(`Down: ${rootString} ${props.chord.name}`);
@@ -25,9 +52,20 @@ function ChordButton(props: { rootMidiValue: number, chord: Chord, preferSharp: 
   function mouseUp() {
   }
 
-  return <button onMouseDown={mouseDown} onMouseUp={mouseUp}>
-    {text}
-  </button>;
+  return (
+    <button
+      className="chord-button"
+      onMouseDown={mouseDown}
+      onMouseUp={mouseUp}
+      style={{
+        gridColumnStart: props.column,
+        gridRowStart: props.row,
+      }}>
+      <span className='button-text'>
+        {/* {text} */}
+        {text}
+      </span>
+    </button>);
 }
 
 export default App;
