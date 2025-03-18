dea0fabda5870771677ba894830798cd980df220
Luna Futures <luna.futures+github@gmail.com>
2025-03-18 00:25:11 -0500

    Create MusicalRangeSlider component and story

--- src/midi.ts
+++ src/midi.ts
@@ -1,12 +1,25 @@
 import { Chord } from "./chords";
 import _ from "lodash";
 
+// There are two midi standards. This file uses the standard where C4 is middle C (instead of C3).
+export const A0_midi = 21;
+export const C3_midi = 48;
+export const C5_midi = 72;
+export const C8_midi = 108;
+
 export class MidiNote {
     constructor(public midiValue: number) { }
 
-    toString(preferSharp: boolean): string {
+    toString(preferSharp: boolean, stylized: boolean = false): string {
         const octave = Math.floor(this.midiValue / 12) - 1;
-        return `${this.rootString(preferSharp)}${octave}`;
+        const combined = `${this.rootString(preferSharp)}${octave}`;
+        if (stylized) {
+            return combined;
+        } else {
+            return combined
+                .replaceAll('♯', "#")
+                .replaceAll('♭', 'b');
+        }
     }
 
     rootString(preferSharp: boolean): string {
@@ -21,19 +34,23 @@ export class MidiNote {
     }
 }
 
+export function midiToString(value: number, preferSharp: boolean, stylized: boolean = false): string {
+    return new MidiNote(value).toString(preferSharp, stylized);
+}
+
 function rootValueToString(value: number, preferSharp: boolean): string {
     switch(value) {
         case 0: return "C";
-        case 1: return preferSharp ? "C#" : "Db";
+        case 1: return preferSharp ? "C♯" : "D♭";
         case 2: return "D";
-        case 3: return preferSharp ? "D#" : "Eb";
+        case 3: return preferSharp ? "D♯" : "E♭";
         case 4: return "E";
         case 5: return "F";
-        case 6: return preferSharp ? "F#" : "Gb";
+        case 6: return preferSharp ? "F♯" : "G♭";
         case 7: return "G";
-        case 8: return preferSharp ? "G#" : "Ab";
+        case 8: return preferSharp ? "G♯" : "A♭";
         case 9: return "A";
-        case 10: return preferSharp ? "A#" : "Bb";
+        case 10: return preferSharp ? "A♯" : "B♭";
         case 11: return "B";
         default:
             throw new RangeError(`Input value (${value}) must be between 0 and 11 inclusive.`);
---
+++ src/stories/RangeSlider.tsx
@@ -0,0 +1,108 @@
+import React, { JSX } from 'react';
+
+import './range-slider.css';
+import { Button, Slider, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { A0_midi, C8_midi, midiToString } from '../midi';
+
+export interface MusicalRangeSliderProps {
+    midiMin: number,
+    midiMax: number,
+    /** Midi values of the initial note values.*/
+    initialValues: [number, number],
+    /** Shows notes as ♯ instead of ♭.*/
+    preferSharp: boolean,
+    /** Shows a # as a ♯, and b as a ♭.*/
+    stylized: boolean,
+    valueLabelDisplay: 'on' | 'auto' | 'off',
+    color: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning',
+    ariaLabel: string,
+    onValuesChanged: (event: Event, value: number | number[], activeThumb: number) => void,
+}
+
+export const MusicalRangeMidiMinDefault = A0_midi;
+export const MusicalRangeMidiMaxDefault = C8_midi;
+
+enum ThumbsToMove {
+    One = "one",
+    Both = "both",
+}
+
+const ThumbsToMoveDefault = ThumbsToMove.Both;
+
+function coerce(value: number, min: number, max: number): number {
+    return Math.max(min, Math.min(value, max));
+}
+
+/** A slider that selects two musical notes. */
+export function MusicalRangeSlider({
+    midiMin = MusicalRangeMidiMinDefault,
+    midiMax = MusicalRangeMidiMaxDefault,
+    initialValues,
+    preferSharp,
+    stylized = true,
+    valueLabelDisplay,
+    color,
+    onValuesChanged,
+    ariaLabel = "Musical Range Slider",
+}: MusicalRangeSliderProps): JSX.Element {
+    const [values, setValues] = React.useState(initialValues);
+    const [thumbsToMove, setThumbsToMove] = React.useState(ThumbsToMoveDefault);
+    return (
+        <div className="range-slider-container">
+            <Slider
+                min={midiMin}
+                max={midiMax}
+                getAriaLabel={() => ariaLabel}
+                value={values}
+                valueLabelFormat={(value: number) => {
+                    return `${midiToString(value, preferSharp, stylized)} (${value})`;
+                }}
+                onChange={function (event: Event, value: number | number[], activeThumb: number) {
+                    console.log("shift?", thumbsToMove, "activeThumb?", activeThumb, "value?", value);
+                    if (value instanceof Array && value.length == 2) {
+                        const [first, second] = value;
+                        if (thumbsToMove == ThumbsToMove.Both) {
+                            setValues(old => {
+                                const [oldFirst, oldSecond] = old;
+                                const delta = (activeThumb == 0)
+                                    ? first - oldFirst
+                                    : second - oldSecond;
+
+                                const deltaUpperLimit = midiMax - Math.max(oldFirst, oldSecond);
+                                const deltaLowerLimit = midiMin - Math.min(oldFirst, oldSecond);
+                                const coercedDelta = coerce(delta, deltaLowerLimit, deltaUpperLimit);
+                                console.log("delta", delta, "deltaLowerLimit", deltaLowerLimit, "deltaUpperLimit", deltaUpperLimit, "coercedDelta", coercedDelta);
+                                return [oldFirst + coercedDelta, oldSecond + coercedDelta];
+                            });
+                        } else {
+                            setValues([first, second]);
+                        }
+                        onValuesChanged(event, value, activeThumb);
+                    }
+                }}
+                valueLabelDisplay={valueLabelDisplay}
+                color={color}
+            />
+
+            <div className='range-slider-auxiliary-settings'>
+                <ToggleButtonGroup
+                    color={color}
+                    value={thumbsToMove}
+                    exclusive
+                    onChange={(_event, newValue: ThumbsToMove) => {
+                        setThumbsToMove(newValue);
+                    }}
+                    aria-label="Platform"
+                >
+                    <ToggleButton value={ThumbsToMove.One}>Move One</ToggleButton>
+                    <ToggleButton value={ThumbsToMove.Both}>Move Both</ToggleButton>
+                </ToggleButtonGroup>
+
+                <Button variant="outlined" onClick={() => {
+                    setThumbsToMove(ThumbsToMoveDefault);
+                    setValues(initialValues);
+                }}> Reset </Button>
+            </div>
+        </div>
+    );
+};
