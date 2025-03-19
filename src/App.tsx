4252e2bc1897b45c8a4860060f75a84d2526fbe4
Luna Futures <luna.futures+github@gmail.com>
2025-03-19 02:04:06 -0500

    Synth should be at top, set volume, stop on mouse up

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { C3_midi, C5_midi, MidiNote } from './midi';
 import { Chord, chords } from './chords';
 import _ from 'lodash';
-import { PolySynth, now as toneNow } from 'tone';
+import { PolySynth, Synth, SynthOptions, now as toneNow } from 'tone';
 import { MusicalRangeMidiMaxDefault, MusicalRangeMidiMinDefault, MusicalRangeSlider } from './stories/RangeSlider';
 import { resumeAudioContext } from './toneManager';
 import { createTheme, ThemeProvider } from '@mui/material';
@@ -14,9 +14,11 @@ const darkTheme = createTheme({
     },
 });
 
+type MySynth = PolySynth<Synth<SynthOptions>>;
 function App(): JSX.Element {
     const initialRange: [number, number] = [C3_midi, C5_midi];
     const [range, setRange] = React.useState(initialRange);
+    const synth: MySynth = React.useMemo(() => new PolySynth({maxPolyphony: 100}).toDestination(), []);
     return (
         <ThemeProvider theme={darkTheme}>
             <div className="App">
@@ -38,13 +40,14 @@ function App(): JSX.Element {
                     sameRootRunsDown={true}
                     rangeLow={range[0]}
                     rangeHigh={range[1]}
+                    synth={synth}
                     />
             </div>
         </ThemeProvider>
     );
 }
 
-function Chords(props: {sameRootRunsDown: boolean, rangeLow: number, rangeHigh: number}): JSX.Element {
+function Chords(props: {sameRootRunsDown: boolean, rangeLow: number, rangeHigh: number, synth: MySynth}): JSX.Element {
     const buttons: JSX.Element[] = [];
     _.range(0, 12).map((rootMidiValue, i) => {
         chords.map((chord, j) => {
@@ -58,6 +61,7 @@ function Chords(props: {sameRootRunsDown: boolean, rangeLow: number, rangeHigh: 
                     row={props.sameRootRunsDown ? j + 1 : i + 1}
                     rangeLow={props.rangeLow}
                     rangeHigh={props.rangeHigh}
+                    synth={props.synth}
                     />);
         });
     });
@@ -75,12 +79,12 @@ function ChordButton(props: {
     column: number,
     row: number,
     rangeLow: number,
-    rangeHigh: number
+    rangeHigh: number,
+    synth: MySynth,
 }): JSX.Element {
     const midiNote = new MidiNote(props.rootMidiValue);
     const rootString = midiNote.rootString(props.preferSharp);
     const abbrChordName = `${rootString}${'\u200B'}${props.chord.abbreviation}`;
-    const synth = React.useMemo(() => new PolySynth({maxPolyphony: 12}).toDestination(), []);
 
     function noteStr(value: number): string {
         return new MidiNote(value).toString(props.preferSharp);
@@ -98,11 +102,14 @@ function ChordButton(props: {
         await resumeAudioContext();
         console.log(`Limiting to notes between ${noteStr(props.rangeLow)} and ${noteStr(props.rangeHigh)}`);
         console.log(`Playing notes: ${notes}`);
-        synth.volume.value = -6;
-        synth.triggerAttackRelease(notes, "1n");
+        // TODO: Handle time differently if in mobile or desktop
+        props.synth.volume.value = -6;
+        props.synth.releaseAll();
+        props.synth.triggerAttack(notes);
     }
 
     function mouseUp(): void {
+        props.synth.releaseAll(toneNow() + 0.3);
     }
 
     return (
