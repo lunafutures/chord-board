df3a17ba1370b2d86ed8f73a30d630ae4e1b9efe
Luna Futures <luna.futures+github@gmail.com>
2025-03-22 00:00:02 -0500

    Rearrange chords into logical groupings

--- src/chords.ts
+++ src/chords.ts
@@ -19,24 +19,6 @@ export const chords: Chord[] = [
         intervals: ['1', 'b3', '5']
     },
     {
-        name: 'Dominant 7',
-        abbreviation: '7',
-        formula: [0, 4, 7, 10],
-        intervals: ['1', '3', '5', 'b7']
-    },
-    {
-        name: 'Major 7',
-        abbreviation: '△7',
-        formula: [0, 4, 7, 11],
-        intervals: ['1', '3', '5', '7']
-    },
-    {
-        name: 'Minor 7',
-        abbreviation: 'm7',
-        formula: [0, 3, 7, 10],
-        intervals: ['1', 'b3', '5', 'b7']
-    },
-    {
         name: 'Diminished',
         abbreviation: 'dim',
         formula: [0, 3, 6],
@@ -61,52 +43,52 @@ export const chords: Chord[] = [
         intervals: ['1', '4', '5']
     },
     {
-        name: 'Minor 7♭5',
-        abbreviation: 'm7♭5',
-        formula: [0, 3, 6, 10],
-        intervals: ['1', 'b3', 'b5', 'b7']
-    },
-    {
-        name: 'Dominant 9',
-        abbreviation: '9',
-        formula: [0, 4, 7, 10, 14],
-        intervals: ['1', '3', '5', 'b7', '9']
-    },
-    {
-        name: 'Minor 9',
-        abbreviation: 'm9',
-        formula: [0, 3, 7, 10, 14],
-        intervals: ['1', 'b3', '5', 'b7', '9']
-    },
-    {
-        name: 'Major 9',
-        abbreviation: '△9',
-        formula: [0, 4, 7, 11, 14],
-        intervals: ['1', '3', '5', '7', '9']
-    },
-    {
         name: 'Power Chord',
         abbreviation: '5',
         formula: [0, 7],
         intervals: ['1', '5']
     },
     {
-        name: 'Dominant 13',
-        abbreviation: '13',
-        formula: [0, 4, 7, 10, 14, 21],
-        intervals: ['1', '3', '5', 'b7', '9', '13']
+        name: 'Sixth',
+        abbreviation: '6',
+        formula: [0, 4, 7, 9],
+        intervals: ['1', '3', '5', '6']
     },
     {
-        name: 'Minor 11',
-        abbreviation: 'm11',
-        formula: [0, 3, 7, 10, 14, 17],
-        intervals: ['1', 'b3', '5', 'b7', '9', '11']
+        name: 'Dominant 7',
+        abbreviation: '7',
+        formula: [0, 4, 7, 10],
+        intervals: ['1', '3', '5', 'b7']
     },
     {
-        name: 'Major 11',
-        abbreviation: '△11',
-        formula: [0, 4, 7, 11, 14, 17],
-        intervals: ['1', '3', '5', '7', '9', '11']
+        name: 'Major 7',
+        abbreviation: '△7',
+        formula: [0, 4, 7, 11],
+        intervals: ['1', '3', '5', '7']
+    },
+    {
+        name: 'Minor 7',
+        abbreviation: 'm7',
+        formula: [0, 3, 7, 10],
+        intervals: ['1', 'b3', '5', 'b7']
+    },
+    {
+        name: 'Minor 7♭5',
+        abbreviation: 'm7♭5',
+        formula: [0, 3, 6, 10],
+        intervals: ['1', 'b3', 'b5', 'b7']
+    },
+    {
+        name: '7♯5',
+        abbreviation: '7♯5',
+        formula: [0, 4, 8, 10],
+        intervals: ['1', '3', '#5', 'b7']
+    },
+    {
+        name: '7♭9',
+        abbreviation: '7♭9',
+        formula: [0, 4, 7, 10, 13],
+        intervals: ['1', '3', '5', 'b7', 'b9']
     },
     {
         name: '7♯9',
@@ -127,25 +109,43 @@ export const chords: Chord[] = [
         intervals: ['1', '3', '5', '9']
     },
     {
-        name: 'Sixth',
-        abbreviation: '6',
-        formula: [0, 4, 7, 9],
-        intervals: ['1', '3', '5', '6']
+        name: 'Dominant 9',
+        abbreviation: '9',
+        formula: [0, 4, 7, 10, 14],
+        intervals: ['1', '3', '5', 'b7', '9']
     },
     {
-        name: '7♭9',
-        abbreviation: '7♭9',
-        formula: [0, 4, 7, 10, 13],
-        intervals: ['1', '3', '5', 'b7', 'b9']
+        name: 'Major 9',
+        abbreviation: '△9',
+        formula: [0, 4, 7, 11, 14],
+        intervals: ['1', '3', '5', '7', '9']
     },
     {
-        name: '7♯5',
-        abbreviation: '7♯5',
-        formula: [0, 4, 8, 10],
-        intervals: ['1', '3', '#5', 'b7']
+        name: 'Minor 9',
+        abbreviation: 'm9',
+        formula: [0, 3, 7, 10, 14],
+        intervals: ['1', 'b3', '5', 'b7', '9']
     },
     {
-        name: 'Quartal',
+        name: 'Major 11',
+        abbreviation: '△11',
+        formula: [0, 4, 7, 11, 14, 17],
+        intervals: ['1', '3', '5', '7', '9', '11']
+    },
+    {
+        name: 'Minor 11',
+        abbreviation: 'm11',
+        formula: [0, 3, 7, 10, 14, 17],
+        intervals: ['1', 'b3', '5', 'b7', '9', '11']
+    },
+    {
+        name: 'Dominant 13',
+        abbreviation: '13',
+        formula: [0, 4, 7, 10, 14, 21],
+        intervals: ['1', '3', '5', 'b7', '9', '13']
+    },
+    {
+        name: 'Quartal Perfect 4th',
         abbreviation: 'quart',
         formula: [0, 5, 10],
         intervals: ['1', '4', 'b7']
